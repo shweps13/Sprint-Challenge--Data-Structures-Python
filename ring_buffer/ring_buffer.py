844e22df62eb9877f589e5ff646879824344b229
Heorhii Siburov <g.siburov@gmail.com>
2020-01-31 09:18:28 -0800

    ringbuffer append works fine

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -10,8 +10,17 @@ class RingBuffer:
     def append(self, item):
         if self.storage.length < self.capacity:
             self.storage.add_to_tail(item)
-            print(self.storage.length)
-
+            self.current = self.storage.tail
+            # print(self.storage.length)
+        else:
+            if len(self.storage) >= self.capacity:
+                # print(self.current, self.storage.tail)
+                if self.current is self.storage.tail:
+                    self.current = self.storage.head
+                    self.storage.head.value = item
+                else:
+                    self.current.next.value = item
+                    self.current = self.current.next
 
     def get(self):
         # Note:  This is the only [] allowed
@@ -25,15 +34,22 @@ class RingBuffer:
         while current:
             list_buffer_contents.append(current.value)
             current = current.next
-        print(list_buffer_contents)
+        # print(list_buffer_contents)
         return list_buffer_contents
 
-buffer = RingBuffer(3)
-buffer.append('a')
-buffer.append('b')
-buffer.append('c')
+# buffer = RingBuffer(3)
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
 
-buffer.get()
+# buffer.get()
+# buffer.append('d')
+
+# buffer.get()
+# buffer.append('e')
+# buffer.append('f')
+
+# buffer.get()
 
 # ----------------Stretch Goal-------------------
 
