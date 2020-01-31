ae6adae07f82e0da64242926c81712131c23954f
Heorhii Siburov <g.siburov@gmail.com>
2020-01-31 09:37:16 -0800

    quicksort was added

--- names/names.py
+++ names/names.py
@@ -1,5 +1,21 @@
 import time
 
+def quicksort_out_of_place(arr):
+    # 1. Pick a pivot and move it until everything
+    # on the left is smaller & everything on the right is greater
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[-1]
+    smaller = []
+    larger = []
+    for i in range(len(arr) - 1):
+        element = arr[i]
+        if element < pivot:
+            smaller.append(element)
+        else:
+            larger.append(element)
+    return quicksort_out_of_place(smaller) + [pivot] + quicksort_out_of_place(larger)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -19,6 +35,9 @@ for name_1 in names_1:
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+# Original runtime is: 9.574947834014893 seconds
+# Runtime complexity is: O(n^2)
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
